fa24fcbc41e34924f54b6524a6c0fc1501f959df
Arturkalangomon <39973544+Arturkalangomon@users.noreply.github.com>
2018-09-20 21:10:03 -0300

    Bombinha em C++
    um programinha pra zuar o pessoal no laboratório, >.-.> :v (o volume do sistema deve estar alto)
    o programa apenas para fins de zueira

---
+++ Bombinha em C.cpp
@@ -0,0 +1,11 @@
+/* pra zuar com o pessoal na aula */
+
+#include <iostream>
+using namespace std;
+
+int main(){
+  for(int i = 0; i <= 10; i++){
+    cout << "Contando: \a" << i << endl;
+    }
+return 0;
+}
