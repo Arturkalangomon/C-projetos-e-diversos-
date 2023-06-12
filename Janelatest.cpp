07972435763e46c6ad04085ac044e1712e82a788
Arturkalangomon <39973544+Arturkalangomon@users.noreply.github.com>
2023-06-12 02:46:54 -0300

    Update Janelatest.cpp

--- Janelatest.cpp
+++ Janelatest.cpp
@@ -1,8 +1,8 @@
 #include <windows.h>
 
 /* This is where all the input to the window goes to */
-/* Isso é aqui onde toda a entrada para a janela vai */
-/* Protópito do procedimento da janela*/
+/* Isso Ã© aqui onde toda a entrada para a janela vai */
+/* ProtÃ³pito do procedimento da janela*/
 LRESULT CALLBACK WndProc(HWND hwnd, UINT Message, WPARAM wParam, LPARAM lParam) {
 	switch(Message) {
 		
@@ -14,7 +14,7 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT Message, WPARAM wParam, LPARAM lParam) 
 		}
 		
 		/* All other messages (a lot of them) are processed using default procedures */
-		/* TODAS as outras mensagens (um monte delas) são preocessadas usando procedimetnos padrões */
+		/* TODAS as outras mensagens (um monte delas) sÃ£o preocessadas usando procedimetnos padrÃµes */
 		default:
 			return DefWindowProc(hwnd, Message, wParam, lParam);
 	}
@@ -22,24 +22,24 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT Message, WPARAM wParam, LPARAM lParam) 
 }
 
 /* The 'main' function of Win32 GUI programs: this is where execution starts */
-/* A função "principal/main" de programas GUI Win32: É aqui que a execução começa */ //GUI Graphic User Interface (interface grafica de usuario))...
+/* A funÃ§Ã£o "principal/main" de programas GUI Win32: Ã‰ aqui que a execuÃ§Ã£o comeÃ§a */ //GUI Graphic User Interface (interface grafica de usuario))...
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
 	WNDCLASSEX wc; /* A properties struct of our window */ //windowsclass wndclass classe da janela...
-	HWND hwnd; /* A 'HANDLE', hence the H, or a pointer to our window */ //identificador , daí o H ou ponteiro nossa janela...
-	MSG msg; /* A temporary location for all messages */ //mensagens... uma localização temporaria para todas as mensagens...
+	HWND hwnd; /* A 'HANDLE', hence the H, or a pointer to our window */ //identificador , daÃ­ o H ou ponteiro nossa janela...
+	MSG msg; /* A temporary location for all messages */ //mensagens... uma localizaÃ§Ã£o temporaria para todas as mensagens...
 
 	/* zero out the struct and set the stuff we want to modify */
 	/* zere a estrutura e defina as coisas que queremos modificar */
 	memset(&wc,0,sizeof(wc));
 	wc.cbSize		 = sizeof(WNDCLASSEX);
-	wc.lpfnWndProc	 = WndProc; /* This is where we will send messages to */
+	wc.lpfnWndProc	 = WndProc; /* This is where we will send messages to */ //Ã‰ aqui que nossas mensagens sÃ£o enviadas para
 	wc.hInstance	 = hInstance;
 	wc.hCursor		 = LoadCursor(NULL, IDC_ARROW);
 	
 	/* White, COLOR_WINDOW is just a #define for a system color, try Ctrl+Clicking it */
 	wc.hbrBackground = (HBRUSH)(COLOR_WINDOW+1); //define a cor de fundo da janela
 	wc.lpszClassName = "WindowClass";
-	wc.hIcon		 = LoadIcon(NULL, IDI_APPLICATION); /* Load a standard icon */ //define um icone padrão...
+	wc.hIcon		 = LoadIcon(NULL, IDI_APPLICATION); /* Load a standard icon */ //define um icone padrÃ£o...
 	wc.hIconSm		 = LoadIcon(NULL, IDI_APPLICATION); /* use the name "A" to use the project icon */ //nome para projeto de icone...
 
 	if(!RegisterClassEx(&wc)) { /* registrando a classe wc (extamente a wndclass no outro programa)*/
@@ -55,7 +55,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 		NULL,NULL,hInstance,NULL);
 
 	if(hwnd == NULL) {
-		MessageBox(NULL, "Window Creation Failed!","Error!",MB_ICONEXCLAMATION|MB_OK); //WindowsClass falha na criação da janela
+		MessageBox(NULL, "Window Creation Failed!","Error!",MB_ICONEXCLAMATION|MB_OK); //WindowsClass falha na criaÃ§Ã£o da janela
 		return 0;
 	}
 
@@ -64,8 +64,8 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 		sent to WndProc. Note that GetMessage blocks code flow until it receives something, so
 		this loop will not produce unreasonably high CPU usage.
 		
-		Este é o coração do nosso programa onde todas a entrada são processadas e enviadas para WndProc.
-		Note que GhetMessage bloqueia o fluxo de código até receber algo, então, ese loop (Laço) não produzirá
+		Este Ã© o coraÃ§Ã£o do nosso programa onde todas a entrada sÃ£o processadas e enviadas para WndProc.
+		Note que GhetMessage bloqueia o fluxo de cÃ³digo atÃ© receber algo, entÃ£o, ese loop (LaÃ§o) nÃ£o produzirÃ¡
 		uso excessivamente alto da CPU.
 	*/
 	while(GetMessage(&msg, NULL, 0, 0) > 0) { /* If no error is received... */
